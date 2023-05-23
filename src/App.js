d3afbdb19a7325e237a789e4b570a81dea086d02
abhishek <98287339+WebDevOliva@users.noreply.github.com>
2023-05-24 00:56:44 +0530

    json tamplate created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import artist from './json/artist.json'
+import song from './json/song.json'
+
 
 function App() {
+  console.log(song);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
