88cc09479fc6ab34bd911e43c3cd0cc1cd0b17fe
Abhishek Yadav <x0abhie@gmail.com>
2023-05-26 09:56:13 +0530

    spotify updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import artist from './json/artist.json'
-import song from './json/song.json'
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="relative App bg-gradient-main w-full h-screen p-2">
+      <div className="rounded-lg" style={{width:"100%",height:"65vh",background:"url('https://images.dailyhive.com/20191231092637/80442074_624403948332761_6833755751653338662_n.jpeg')",backgroundPosition:"top", backgroundSize:"cover", backgroundRepeat:"no-repeat"}}>
+        
+      </div>
+      <div className="bag">
+
+      </div>
     </div>
   );
 }
